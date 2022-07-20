bd23887aca2beb789c224c8b52d8f2ccaee56366
Thaina R Zanfolin <thaina.zanfolin@gmail.com>
2022-07-20 12:36:02 -0300

    organizando pastas e subindo novo projeto - nutricionista

---
+++ 003 projeto-nutri-alura/js/principal.js
@@ -0,0 +1,16 @@
+//mudando H1
+var titulo = document.querySelector(".titulo");
+titulo.textContent = "Positive - Clínica de Saúde | Fichas Nutricionais";
+
+//calculando IMC
+var paciente = document.querySelector("#primeiro-paciente"); //selecionando local que vai pegar (classe acrescentada no html)
+var tdpeso = document.querySelector(".info-peso"); //pegando apenas a classe info-peso dentro do primeiro paciente e colocando em tdpeso
+var peso = tdpeso.textContent; //pegando apenas o conteudo dentro da classe info-peso, que está na variavel tdpeso
+var tdaltura = document.querySelector(".info-altura");
+var altura = tdaltura.textContent;
+
+var imc = peso/(altura*altura); //calculo do imc
+
+console.log(peso); //mostrando apenas o textContent no console
+console.log(altura);
+console.log("IMC:" + imc); //+ para concatenar a string com o resultado do calculo
---
+++ 003 projeto-nutri-alura/readme.md
@@ -0,0 +1,10 @@
+# Projeto Nutricionista
+
+Olá! Tudo bem? <br>
+Esse é mais um projeto que estou fazendo para <b>aprender e treinar</b> JavaScript! </b>
+
+A cada novo projeto tento sempre melhorar meu código, tornando mais claro e limpo! <br>
+
+* Projeto em andamento...
+* Os comentários nos códigos são apenas para aprendizado, por isso não seguem as boas práticas de clean code ;)
+* Feito a partir do curso de JavaScript da Alura <b>JavaScript: programando em linguagem da web</b>
