695cf0c89fe0ece8e7a858bd3420552300694f23
Thaina R Zanfolin <thaina.zanfolin@gmail.com>
2022-07-21 08:28:53 -0300

    continuando projeto - logica em javascript

--- 003 projeto-nutri-alura/js/principal.js
+++ 003 projeto-nutri-alura/js/principal.js
@@ -8,8 +8,26 @@ var tdpeso = document.querySelector(".info-peso"); //pegando apenas a classe inf
 var peso = tdpeso.textContent; //pegando apenas o conteudo dentro da classe info-peso, que está na variavel tdpeso
 var tdaltura = document.querySelector(".info-altura");
 var altura = tdaltura.textContent;
+var tdImc = paciente.querySelector(".info-imc"); //colocando o calculo na tabela
 
-var imc = peso/(altura*altura); //calculo do imc
+var pesoValido = true;
+var alturaValida= true; 
+
+    if(peso<=0 || peso>=250){
+        pesoValido = false;
+        tdImc.textContent = "Peso inválido"
+    }
+    
+    if (altura<=0 || altura>=3.0){
+        alturaValida=false;
+        tdImc.textContent = "Altura inválida"
+    }
+   
+    if (pesoValido==true && alturaValida==true){
+        var imc = peso/(altura*altura); //calculo do imc
+        tdImc.textContent = imc;
+    }
+  
 
 console.log(peso); //mostrando apenas o textContent no console
 console.log(altura);
